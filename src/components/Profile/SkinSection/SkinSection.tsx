f1e797f09fe8676b5aa91a7a65dc14c9e629a884
mikinol <sm44aksdmiki13877kfh@gmail.com>
2025-04-15 18:56:13 +0300

    Дописал кнопку

--- src/components/Profile/SkinSection/SkinSection.tsx
+++ src/components/Profile/SkinSection/SkinSection.tsx
@@ -1,11 +1,11 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import style from "./style.module.css"
 import MinecraftSkinViewer from "../../SkinsLib/MinecraftSkinViewer.tsx";
 
- export const SkinSection: React.FC = () => {
-     const [handType, setHandType] = useState(true);
+export const SkinSection: React.FC = () => {
+    const [handType, setHandType] = useState(true);
 
-     const onClick: () => void = ()=>  {
+    const onClick: () => void = () => {
         if (handType) {
             setHandType(false);
             // TODO: сделать обработку на сервере
@@ -15,7 +15,15 @@ import MinecraftSkinViewer from "../../SkinsLib/MinecraftSkinViewer.tsx";
             // TODO: сделать обработку на сервере
             console.log(handType);
         }
-     }
+    }
+
+    function handleFileChange(e: any) {
+        const file = e.target.files[0];
+        if (file && !file.name.endsWith('.png')) {
+            alert('Пожалуйста, выбери .png файл!');
+            e.target.value = ''; // сбросить выбранный файл
+        }
+    }
 
     return (<section className={style.skin_wrapper}>
         <div className={style.skin}>
@@ -25,7 +33,10 @@ import MinecraftSkinViewer from "../../SkinsLib/MinecraftSkinViewer.tsx";
         </div>
         <div className={style.button_wrapper}>
             <button onClick={onClick} className={style.button}>{handType ? "Default" : "Slim"}</button>
-            <input type="file" className={style.button} placeholder="Загрузить скин"/>
+            <label className={style.input}>
+                <input type="file" accept=".png" hidden onChange={handleFileChange} />
+                Загрузить скин
+            </label>
             <button className={style.button}>Установить плащ</button>
         </div>
     </section>)
